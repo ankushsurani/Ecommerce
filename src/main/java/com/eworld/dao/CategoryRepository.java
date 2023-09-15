98edc2592602472cb036d8d76d65c75ab688cb6f
ankushsurani <ankushsurani@gmail.com>
2023-09-15 20:37:59 +0530

    fix in category delete from view

--- src/main/java/com/eworld/dao/CategoryRepository.java
+++ src/main/java/com/eworld/dao/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.eworld.dao;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +16,10 @@ public interface CategoryRepository extends JpaRepository<Category, String> {
 	@Query("SELECT c.title FROM Category c WHERE c.id = :id")
 	public String findTitleById(String id);
 
+	@Query("SELECT c FROM Category c WHERE c.active=true")
 	public Page<Category> findAll(Pageable pageable);
+	
+	@Query("SELECT c FROM Category c WHERE c.active=true")
+	public List<Category> findAll();
 
 }
